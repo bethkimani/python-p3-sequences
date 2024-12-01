2c461aa52f64f8ade6abce7748b30c74b2c43bc1
Beth Kimani <kbwaceke@gmail.com>
2024-12-01 20:06:35 +0300

    added Fib_Sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")  # Print empty list as string to match the test
+        return
+
+    # Start the sequence
+    fib_sequence = [0, 1]
+    
+    # Generate the sequence for length > 2
+    for i in range(2, length):
+        fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+    
+    # Print the sequence up to the specified length
+    print(fib_sequence[:length])
